4e1ee47700e56764b9f1d1d881fd49af03c26c18
Takuya Asano <takuya.a@gmail.com>
2014-12-10 02:22:03 +0900

    refactor: devide Tokenizer.prototype.tokenizeForSentence() function to multiples

--- src/Tokenizer.js
+++ src/Tokenizer.js
@@ -75,43 +75,67 @@ Tokenizer.prototype.tokenize = function (text) {
     return tokens;
 };
 
+/**
+ * Tokenize sentence
+ * @param {string} sentence Sentence splitted by punctuation
+ * @param {Array} tokens tokens Already computed tokens array
+ * @returns {Array} Tokens concatenated with given tokens array
+ */
 Tokenizer.prototype.tokenizeForSentence = function (sentence, tokens) {
+    var lattice = this.getLattice(sentence);
+    return this.getBestPath(lattice, tokens);
+};
+
+/**
+ * Get shortest path tokens by Viterbi algorithm
+ * @param {ViterbiLattice} lattice Lattice to compute
+ * @param {Array} tokens Already computed tokens array
+ * @returns {Array} Tokens concatenated with given tokens array
+ */
+Tokenizer.prototype.getBestPath = function (lattice, tokens) {
     if (tokens == null) {
         tokens = [];
     }
-    var lattice = this.getLattice(sentence);
     var best_path = this.viterbi_searcher.search(lattice);
 
     for (var j = 0; j < best_path.length; j++) {
         var node = best_path[j];
-
-        var token, features, features_line;
-        if (node.type === "KNOWN") {
-            features_line = this.token_info_dictionary.getFeatures(node.name);
-            if (features_line == null) {
-                features = [];
-            } else {
-                features = features_line.split(",");
-            }
-            token = this.formatter.formatEntry(node.name, node.start_pos, node.type, features);
-        } else if (node.type === "UNKNOWN") {
-            // Unknown word
-            features_line = this.unknown_dictionary.getFeatures(node.name);
-            if (features_line == null) {
-                features = [];
-            } else {
-                features = features_line.split(",");
-            }
-            token = this.formatter.formatUnknownEntry(node.name, node.start_pos, node.type, features, node.surface_form);
-        } else {
-            // TODO User dictionary
-            token = this.formatter.formatEntry(node.name, node.start_pos, node.type, []);
-        }
-
+        var token = this.formatNode(node);
         tokens.push(token);
     }
-
     return tokens;
+};
+
+/**
+ * Format ViterbiNode to token
+ * @param {ViterbiNode} node
+ * @returns {*} JSON token constructed by formatter
+ */
+Tokenizer.prototype.formatNode = function (node) {
+    var token, features, features_line;
+    if (node.type === "KNOWN") {
+        features_line = this.token_info_dictionary.getFeatures(node.name);
+        if (features_line == null) {
+            features = [];
+        } else {
+            features = features_line.split(",");
+        }
+        token = this.formatter.formatEntry(node.name, node.start_pos, node.type, features);
+    } else if (node.type === "UNKNOWN") {
+        // Unknown word
+        features_line = this.unknown_dictionary.getFeatures(node.name);
+        if (features_line == null) {
+            features = [];
+        } else {
+            features = features_line.split(",");
+        }
+        token = this.formatter.formatUnknownEntry(node.name, node.start_pos, node.type, features, node.surface_form);
+    } else {
+        // TODO User dictionary
+        token = this.formatter.formatEntry(node.name, node.start_pos, node.type, []);
+    }
+
+    return token;
 };
 
 /**
