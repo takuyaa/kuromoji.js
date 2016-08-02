169174e8f381eeb5f37645b08449c09ed975ab60
Takuya Asano <takuya.a@gmail.com>
2016-08-03 00:37:38 +0900

    chore: add copyright text

--- src/loader/BrowserDictionaryLoader.js
+++ src/loader/BrowserDictionaryLoader.js
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2014 Takuya Asano
+ * Copyright 2010-2014 Atilika Inc. and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var zlib = require("zlibjs/bin/gunzip.min.js");
 var DictionaryLoader = require("./DictionaryLoader");
+
 /**
  * BrowserDictionaryLoader inherits DictionaryLoader, using jQuery XHR for download
  * @param {string} dic_path Dictionary path
@@ -8,8 +26,8 @@ var DictionaryLoader = require("./DictionaryLoader");
 function BrowserDictionaryLoader(dic_path) {
     DictionaryLoader.apply(this, [dic_path]);
 }
+
 BrowserDictionaryLoader.prototype = Object.create(DictionaryLoader.prototype);
-// BrowserDictionaryLoader.prototype.constructor = BrowserDictionaryLoader;
 
 /**
  * Utility function to load gzipped dictionary
