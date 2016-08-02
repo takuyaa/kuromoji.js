429c9b394cb3905c44c895575eab3e31431d0823
Takuya Asano <takuya.a@gmail.com>
2016-08-03 01:18:21 +0900

    remove commented out code

--- src/dict/UnknownDictionary.js
+++ src/dict/UnknownDictionary.js
@@ -26,7 +26,6 @@ var ByteBuffer = require("../util/ByteBuffer");
  * @constructor
  */
 function UnknownDictionary() {
-    // TokenInfoDictionary.apply(this);  // execute super class constructor
     this.dictionary = new ByteBuffer(10 * 1024 * 1024);
     this.target_map = {};  // class_id (of CharacterClass) -> token_info_id (of unknown class)
     this.pos_buffer = new ByteBuffer(10 * 1024 * 1024);
@@ -35,7 +34,6 @@ function UnknownDictionary() {
 
 // Inherit from TokenInfoDictionary as a super class
 UnknownDictionary.prototype = Object.create(TokenInfoDictionary.prototype);
-// UnknownDictionary.prototype.constructor = UnknownDictionary;
 
 UnknownDictionary.prototype.characterDefinition = function (character_definition) {
     this.character_definition = character_definition;
